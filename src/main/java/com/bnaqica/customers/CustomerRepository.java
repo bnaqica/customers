2deb060351e5fa7b1e73539db8b7562a098ed089
Brian Naqica <brian.naqica@Brians-MacBook-Pro.local>
2019-01-01 20:47:58 -0700

    Fixed Repository error

--- src/main/java/com/bnaqica/customers/CustomerRepository.java
+++ src/main/java/com/bnaqica/customers/CustomerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
 }
